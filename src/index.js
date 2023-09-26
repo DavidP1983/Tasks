89fbd3d64aa4c4775eab6dff4b414ef591c92ec3
David <David.Piruzashvili@gmail.com>
2023-09-26 11:13:44 +0400

    Tasks-Object and Array

--- src/index.js
+++ src/index.js
@@ -214,3 +214,279 @@ function calculateVolumeAndArea(cubeEdgeLenght, sq = 6) {
 }
 
 console.log(calculateVolumeAndArea(5));
+
+
+
+// 5 Функция принимает только целое число от 1 до 36. Если переданный аргумент не число, отрицательное или дробное - возвращается сообщение: "Ошибка. Проверьте правильность введенного номера места"Если число 0 или больше 36, то сообщение: "Таких мест в вагоне не существует" getCoupeNumber(33)  => 9
+
+function getCoupeNumber(num) {
+    if (num < 0 || !Number.isInteger(num)) {
+        return `Error. Check that the seat number entered is correct`;
+    }
+
+    if (num == 0 || num > 36) {
+        return `Places like this doesn't exist`;
+    }
+    const couperNumber = Math.round(num / 4);
+    return couperNumber;
+}
+
+console.log(getCoupeNumber(36));
+
+
+
+// 6 Создайте функцию, которая принимает в себя целое число минут и возвращает время в нужном формате строки.
+
+function getTimeFromMinutes(num) {
+    if (num < 0 || !Number.isInteger(num)) {
+        return `Error, check your data`;
+    }
+    const hour = Math.floor(num / 60);
+    const munites = num - (hour * 60);
+    let str = '';
+    switch (hour) {
+        case 1:
+            str += `${hour} час`;
+            break;
+        case 2:
+        case 3:
+        case 4:
+            str += `${hour} часа`;
+            break;
+        case 0:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+            str += `${hour} часов`;
+            break;
+        default:
+            str += `error`;
+    }
+    return `Time: ${str} ${munites} минут`;
+}
+
+console.log(getTimeFromMinutes(15));
+
+
+
+// 7 Напишите функцию, которая принимает в себя 4 числа и возвращает самое большее из них. Если один из аргументов не является числом или их меньше 4 - возвращается 0. findMaxNumber(1, 5, 6.6, 11); =>  11
+
+function findMaxNumber(...num) {
+    for (let i = 0; i < num.length; i++) {
+        if (typeof (num[i]) != 'number' || num.length < 4) {
+            return 0;
+        }
+    }
+    const maxNumber = Math.max(...num);
+    return maxNumber;
+}
+
+console.log(findMaxNumber(1, 5, 12, 11));
+
+
+
+// 8 числа Фибоначчи fib(4) => "0 1 1 2" , fib(7) => "0 1 1 2 3 5 8"
+
+function fib(num) {
+    let str = '0 1';
+    let num1 = 0;
+    let num2 = 1;
+    let num3;
+    for (let i = 2; i < num; i++) {
+        num3 = num1 + num2;
+        num1 = num2;
+        num2 = num3;
+        str += ` ${num2}`;
+
+    }
+    return str;
+
+}
+
+console.log(fib(7));
+
+
+
+
+// --- Задачи на работу с объектами --- //
+
+
+// 1 Напишите функцию showExperience, которая будет принимать в себя объект со всеми данными и возвращать строку с опытом. showExperience(personalPlanPeter) => '1 month'
+
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+
+};
+
+function showExperience(plan) {
+    const { skills } = plan;
+    return `${skills.exp}`;
+}
+
+
+console.log(showExperience(personalPlanPeter));
+
+
+
+// 2 Напишите функцию showProgrammingLangs, которая будет принимать в себя объект со всеми данными и возвращать строку в нужном виде.Пример: showProgrammingLangs(personalPlanPeter)  => "Язык js изучен на 20% Язык php изучен на 10%". Причем функция должна работать вне зависимости от количества языков. Если ни один не указан, то возвращается пустая строка.
+
+
+function showProgrammingLangs(plan) {
+    const { programmingLangs } = plan.skills;
+    let str = '';
+    if (Object.keys(programmingLangs).length === 0) {
+        return `The Object is empty`;
+    }
+    for (let i in programmingLangs) {
+        str += `Язык ${i} изучен на ${programmingLangs[i]} \n`;
+    }
+    return str;
+}
+
+console.log(showProgrammingLangs(personalPlanPeter));
+
+
+// 3 Создайте метод showAgeAndLangs внутри объекта personalPlanPeter. При его вызове метод будет принимать в себя объект и возвращать строку в нужном виде. Пример: personalPlanPeter.showAgeAndLangs(personalPlanPeter) => 'Мне 29 и я владею языками: RU ENG'
+
+
+const personalPlanPeter1 = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLans: function (plan) {
+        const { languages } = plan.skills;
+        const { age } = plan;
+        let str = `I'm ${age} and i speak: `;
+        // const str = [...languages];
+        // const [first, second] = languages;
+        // return `I'm ${plan.age} and i speak: ${first.toUpperCase()} ${second.toUpperCase()}`;
+        languages.forEach(lang => {
+            str += `${lang.toUpperCase()} `;
+        });
+        return str;
+    }
+
+};
+
+
+console.log(personalPlanPeter1.showAgeAndLans(personalPlanPeter1));
+
+
+
+// 4 Напишите функцию showFamily, которая будет принимать в себя массив строк и возвращать сообщение в нужном формате. showFamily(family)  => 'Семья состоит из: Peter Ann Alex Linda' Имена подставляются автоматически из массива. Если массив пустой, то выводится сообщение 'Семья пуста'
+
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+// function showFamily(arr) {
+//     if(!arr.length) {
+//         return 'Семья пуста';
+//     }
+//     const result = arr.join(', ');
+//     return `Семья состоит из: ${result}`;
+// }
+
+function showFamily(arr) {
+    let str = '';
+
+    arr.length === 0 ? str = 'Семья пуста' : str = 'Семья состоит из: ';
+    arr.forEach(person => {
+        str += `${person} `;
+    });
+    return str;
+}
+
+
+console.log(showFamily(family));
+
+
+
+// 5 напишите функцию standardizeStrings, которая будет принимать в себя массив строк и будет выводить в консоль эти строки в нижнем регистре.
+
+// Пример:
+
+// standardizeStrings(favoriteCities)  выведет в консоль
+
+// lisbon
+// rome
+// milan
+// dublin
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+
+function standardizeStrings(arr) {
+    let str = '';
+    arr.forEach(cities => {
+        str += `${cities.toLowerCase()}\n`;
+    });
+    return str;
+}
+
+console.log(standardizeStrings(favoriteCities));
+
+
+// 6  Задача с собеседований. Напишите функцию reverse, которая принимает в себя строку и возвращает эту строку в обратном порядке.Пример: const someString = 'This is some strange string'; reverse(someString) => 'gnirts egnarts emos si sihT'
+
+const someString = 'This is some strange string';
+
+function reverse(str) {
+    if (typeof (str) !== 'string') return 'Error';
+
+    let reversStr = '';
+    const arr = str.split(' ').reverse();
+    // const arr = str.split('').reverse();
+    arr.forEach(item => {
+        let newStr = '';
+        for (let i = item.length; i > 0; i--) {
+            newStr += item[i - 1];
+
+        }
+        reversStr += `${newStr} `;
+    });
+    return reversStr;
+    // return arr.join('');
+}
+
+console.log(reverse(someString));
+
+
+
+// 7 Вам нужно создать главную функцию банкомата availableCurr, которая принимает два аргумента: первый - это массив со всеми доступными валютами из двух банков сразу (сейчас представим, что они не могут повторяться), второй - необязательный аргумент, который указывает ту валюту, которая сейчас закончилась в банкомате. Если массив в первом аргументе пустой - то функция возвращает строку 'Нет доступных валют'. Функция возвращает строку в нужном виде. 
+
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(available, missing = 'UAH') {
+    let str = '';
+    available.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты: \n';
+
+    available.forEach(currency => {
+        if (currency !== missing) {
+            str += `${currency}\n`;
+        }
+    });
+    return str;
+}
+
+
+console.log(availableCurr([...baseCurrencies, ...additionalCurrencies]));
