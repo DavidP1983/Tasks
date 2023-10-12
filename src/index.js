f91aab7d505cefa5c9a321a063a4bcc2ed778c54
David <David.Piruzashvili@gmail.com>
2023-10-12 16:20:21 +0400

    Array Methods Tasks

--- src/index.js
+++ src/index.js
@@ -820,3 +820,135 @@ function deepCount(arr) {
 }
 
 console.log(deepCount(["1", 5, "3", ["10"]]));
+
+
+
+// --- Array Methods --- //
+
+
+//1 У вас есть список фильмов с рейтингом в виде массива объектов. Напишите функцию showGoodFilms, которая будет принимать этот массив, а возвращать будет массив объектов только с теми фильмами, у которых рейтинг больше или равен 8.
+
+
+
+const films = [
+    {
+        name: 'Titanic',
+        rating: 9
+    },
+    {
+        name: 'Die hard 5',
+        rating: 5
+    },
+    {
+        name: 'Matrix',
+        rating: 8
+    },
+    {
+        name: 'Some bad film',
+        rating: 4
+    }
+];
+
+function showGoodFilms(films) {
+    return films.filter(item => item.rating >= 8);
+}
+
+console.log(showGoodFilms(films));
+
+
+// 2 Напишите функцию showListOfFilms, которая будет принимать этот же массив, а возвращать будет строку, которая содержит названия фильмов через запятую. showListOfFilms(films) => "Titanic, Die hard 5, Matrix, Some bad film"
+
+
+function showListOfFilms(list) {
+    return list.reduce((list, curr) => `${typeof (list) === 'object' ? list.name : list}, ${curr.name}`);
+    // return list.map(item => item.name).join(', ');
+
+}
+
+console.log(showListOfFilms(films));
+
+
+// 3 Напишите функцию setFilmsIds, которая будет принимать этот же массив, а возвращать будет такой же массив с фильмами, но у каждого фильма будет новое поле id. Значение этого поля установите по нумерации фильма.
+// Пример:
+// setFilmsIds(films)  => [   { name: 'Titanic', rating: 9, id: 0 },   { name: 'Die hard 5', rating: 5, id: 1 },   { name: 'Matrix', rating: 8, id: 2 },   { name: 'Some bad film', rating: 4, id: 3 } ]
+
+
+function setFilmsIds(arr) {
+    return arr.map((item, i) => {
+        item.id = i;
+        return item;
+    });
+}
+
+console.log(setFilmsIds(films));
+
+
+
+// 4 Запишите результат предыдущей функции в переменную tranformedArray. Напишите функцию checkFilms, которая будет проверять, что в каждом из фильмов есть поле id. Если это так - функция возвращает true. Очевидно, что сейчас условие должно выполняться, если мы передаем checkFilms(tranformedArray); 
+
+const tranformedArray = setFilmsIds(films);
+
+function checkFilms(arr) {
+    return arr.every(item => item.hasOwnProperty('id'));
+}
+
+console.log(checkFilms(tranformedArray));
+
+
+
+
+// 5 У вас есть небольшой массив с данными о доходах каждой торговой точки. Напишите функцию getPositiveIncomeAmount, которая принимает этот массив данных и возвращает сумму только положительных значений из каждого объекта. (число)
+// Пример:
+// getPositiveIncomeAmount(funds) => 13300
+
+
+const funds = [
+    { amount: -1400 },
+    { amount: 2400 },
+    { amount: -1000 },
+    { amount: 500 },
+    { amount: 10400 },
+    { amount: -11400 }
+];
+
+
+// function getPositiveIncomeAmount(funds) {
+//     return funds.filter(item => item.amount > 0)
+//         .map(item => Object.values(item)[0])
+//         .reduce((sum, curr) => sum + curr);
+// }
+
+function getPositiveIncomeAmount(funds) {
+    return funds.filter(item => item.amount > 0)
+        .reduce((sum, curr) => sum + curr.amount, 0);
+}
+
+console.log(getPositiveIncomeAmount(funds));
+
+
+
+
+// 6 Напишите функцию getTotalIncomeAmount, которая тоже принимает этот массив данных. Если хотя бы один из объектов содержит отрицательное значение поля amount, то функция возвращает сумму всех значений. (число) Если таких значений нет - запускается функция getPositiveIncomeAmount с тем же массивом данных.
+
+// Пример:
+
+// getTotalIncomeAmount(funds) => -500
+
+
+// function getTotalIncomeAmount(funds) {
+//     const hasNegativeObj = funds.some(item => item.amount < 0);
+//     if (hasNegativeObj) {
+//         return funds.reduce((sum, curr) => sum + curr.amount, 0);
+//     } else {
+//         return getPositiveIncomeAmount(funds);
+//     }
+// }
+
+
+function getTotalIncomeAmount(funds) {
+    return funds.some(item => item.amount < 0) ? funds.reduce((sum, curr) => sum + curr.amount, 0) : getPositiveIncomeAmount(funds);
+
+}
+
+console.log(getTotalIncomeAmount(funds));
+
