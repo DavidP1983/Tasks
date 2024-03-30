31497c6e555d32be6d1a993097b109fd2f9fc9ab
David <David.Piruzashvili@gmail.com>
2024-03-31 01:06:58 +0400

    create a global flatten method

--- flatten method/index.js
+++ flatten method/index.js
@@ -15,4 +15,4 @@ function flatten(...arr) {
 }
 
 console.log(flatten(1, [2, 3], 4, 5, [6, [7]]));    // [1, 2, 3, 4, 5, 6, 7]
-console.log(flatten('a', ['b', 2], 3, null, [[4], ['c']])); // ['a', 'b', 2, 3, null, 4, 'c']
+// console.log(flatten('a', ['b', 2], 3, null, [[4], ['c']])); // ['a', 'b', 2, 3, null, 4, 'c']
