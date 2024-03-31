248624e46dcb97d0a06d362a94d12e3b9865910c
David <David.Piruzashvili@gmail.com>
2024-03-31 16:30:23 +0400

    create a global flatten method

--- flatten method/index.js
+++ flatten method/index.js
@@ -1,5 +1,6 @@
 // For this exercise you will create a global flatten method. The method takes in any number of arguments and flattens them into a single array
 
+// 1
 function flatten(...arr) {
 
     return arr.reduce((acc, curr) => {
@@ -13,6 +14,26 @@ function flatten(...arr) {
     }, []);
 
 }
+
+
+//2
+function flatten(...stack) {
+    const flat = [];
+
+    while (stack.length) {
+        const elem = stack.pop();
+        if (Array.isArray(elem)) {
+            stack.push(...elem);
+            continue;
+        }
+        flat.unshift(elem);
+
+    }
+
+    return flat;
+}
+
+
 
 console.log(flatten(1, [2, 3], 4, 5, [6, [7]]));    // [1, 2, 3, 4, 5, 6, 7]
 // console.log(flatten('a', ['b', 2], 3, null, [[4], ['c']])); // ['a', 'b', 2, 3, null, 4, 'c']
